301d85e1096db0492b31a4297d1789a969921db5
TysonHentegs-12 <821583@mystma.org>
2022-09-20 15:12:35 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #WRITE YOUR CODE IN THIS FILE
+from doctest import TestResults
+
+
+def greaterThan(x,y):
+    if x>y:
+        return True
+    else:
+        return False
+def lesserThan(x,y):
+    if x<y:
+        return True
+    else:
+        return False
+def equalTo(x,y):
+    if x==y:
+        return True
+    else:
+        return False
+def greaterOrEqual(x,y):
+    if x>=y:
+        return True
+    else:
+        return False
+def lesserOfEqual(x,y):
+    if x<=y:
+        return True
+    else:
+        return False
